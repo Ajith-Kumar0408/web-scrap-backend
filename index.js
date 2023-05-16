269fa250b0230425be342f9f1b6e88b5db1b35b3
ajith kumar <ajithak0093@gmail.com>
2023-05-16 14:54:25 +0530

    end-backend

---
+++ index.js
@@ -0,0 +1,8 @@
+const browserObject = require('./browser');
+const scraperController = require('./pageController');
+
+
+let browserInstance = browserObject.startBrowser();
+
+
+scraperController(browserInstance)
